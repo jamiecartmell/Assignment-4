6aae8a5b7e08e4acbe48c42144a316481c9984ea
jamie cartmell <jamie@wordpressupdates.ie>
2023-11-14 15:16:50 +0000

    ss

--- config.py
+++ config.py
@@ -4,6 +4,4 @@ import re
 SECRET_KEY = os.getenv("SECRET_KEY", "not-set")
 
 # When deploying, set in the environment to the PostgreSQL URL
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
-    "DATABASE_URL", "sqlite:///db.sqlite3"
-)
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
